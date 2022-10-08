d1977281c78a4e45307aed3e4a36c6d22871e9e6
lijun940 <1058201727@qq.com>
2022-10-08 13:39:44 +0800

    2

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js')
   
-  console.log(baseConfig,'测试')
+  console.log(baseConfig,'测试1')
   it('entry', () => {
     assert.equal(baseConfig.entry.index, 'D:/worklab/practice/深入webpack/webpack/builder-webpack/test/smoke/template/src/index/index.js')
     assert.equal(baseConfig.entry.search, 'D:/worklab/practice/深入webpack/webpack/builder-webpack/test/smoke/template/src/search/index.js')
